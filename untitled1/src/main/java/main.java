841b631df6d80468dbd2b67b556b4a0ae25a2874
Nikita805 <131763991+Nikita805@users.noreply.github.com>
2023-04-25 13:08:16 +0300

    Add files via upload

---
+++ untitled1/src/main/java/main.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Введите первое число");
+            Scanner scanner = new Scanner(System.in);
+            float a = scanner.nextFloat();
+            System.out.println("Введите знак действия + - * /");
+            char n = scanner.next().charAt(0);
+            System.out.println("Введите второе число");
+            float b = scanner.nextFloat();
+                if (n == '+') {
+                    float result = a + b;
+                    System.out.println(a + " + " + b + " = " + result);
+                }
+                if (n == '-') {
+                    float result = a - b;
+                    System.out.println(a + " - " + b + " = " + result);
+                }
+                if (n == '*') {
+                    float result = a * b;
+                    System.out.println(a + " * " + b + " = " + result);
+                }
+                if (n == '/') {
+                    if (n==0) {
+                        System.out.println("Деление на ноль невозможно");
+                    } else {
+                    float result = a / b;
+                    System.out.println(a + " / " + b + " = " + result);
+                }
+
+            }
+        }
+    }
+}
+
